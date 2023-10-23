61071671e1173f5a6fff6897a5e13800d368cd7d
Jody-Ann <drjodyannjones@mgmail.com>
2023-10-23 08:04:20 +0200

    Update data_transformation and data_ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,14 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from pathlib import Path
+from src.components.data_transformation import (
+    DataTransformation,
+    DataTransformationConfig,
+)
+
+
+sys.path.append(str(Path(__file__).parent.parent))
 
 
 @dataclass
@@ -55,4 +63,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    train_arr, test_arr = data_transformation.initiate_data_transformation(
+        train_data, test_data
+    )
